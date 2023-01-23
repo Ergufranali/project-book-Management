e953163a417d770703e342553cfd126632f8590d
BornSava9e <Parasanand1999@gmail.com>
2023-01-23 16:31:38 +0530

    paras

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -4,7 +4,22 @@ const router = require('../routes/route')
 
 
 const createBook =async function(req,res){
+    let data = req.body
+    if(Object.keys(data).length ===0) return res.status(400).send({status : false, message : "Please provide data"})
 
+    const {title, excerpt, userId, ISBN, category, subcategory, releasedAt} = data
+
+    if(!title) return res.status(400).send({ status : false , message : "title is mandatory"})
+    if(typeof title !=="string") return res.status(400).send({status : false , message : "title will only accept string"})
+    if(!title.trim()) return res.status(400).send({status : false , message : "title is mandatory"})
+  
+    if(!excerpt) return res.status(400).send({ status : false , message : "excerpt is mandatory"})
+    if(typeof title !=="string") return res.status(400).send({status : false , message : "excerpt will only accept string"})
+
+    
+
+    let savedata = await bookModel.create(data)
+    res.send({data : savedata})
 }
 
 const getBooks= async function(req,res){
